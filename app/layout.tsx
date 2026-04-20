3104dd1317db14db1202610c29f5893e1c9c1cf7
sakib al hasan <sakibalhasan@sakibs-MacBook-Air.local>
2026-04-21 03:01:17 +0600

    meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover, interactive-widget=resizes-content"
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Mochiy+Pop+One&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Rethink+Sans:ital,wght@0,400..800;1,400..800&display=swap"
